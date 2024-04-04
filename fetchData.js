a91d6cce09d3b6ba5c261d800183ba544e93d0ca
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2024-04-04 11:23:15 -0300

    Add debugging logs, set download dir, list contents

--- fetchData.js
+++ fetchData.js
@@ -1,4 +1,7 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
+
 (async () => {
     function delay(time) {
         return new Promise(function (resolve) {
@@ -7,11 +10,24 @@ const puppeteer = require('puppeteer');
     }
     try {
         // Launch the browser
+        console.log('launching browser')
         const browser = await puppeteer.launch({ headless: true });
+
+        console.log('creating new page')
         const page = await browser.newPage();
+
+        console.log('setting viewport')
         await page.setViewport({ width: 1280, height: 720 });
 
+        console.log('setting download directory')
+        const client = await page.target().createCDPSession()
+        await client.send('Page.setDownloadBehavior', {
+            behavior: 'allow',
+            downloadPath: './jstor_download',
+        })
+
         // Navigate to the login page
+        console.log('logging in')
         await page.goto('https://forum.jstor.org/#/login/by-email');
         // Enter login information
         await page.type('#username', process.env.JSTOR_USER);
@@ -19,10 +35,12 @@ const puppeteer = require('puppeteer');
         await page.click('#loginButton');
 
         // Go to project page
+        console.log('navigating to project page')
         await page.waitForNavigation();
         await page.goto(process.env.JSTOR_PROJECT)
-        
+
         // Select checkbox
+        console.log('selecting items')
         const checkboxSelector = ".ag-header-select-all:nth-child(1)"
         await page.waitForSelector(checkboxSelector);
         await delay(2000)
@@ -35,9 +53,17 @@ const puppeteer = require('puppeteer');
         await page.click(selectAllSelector)
 
         // Export data
+        console.log('exporting data')
         const dropdownSelector = ".more-actions-panel .btn"
         await page.waitForSelector(dropdownSelector)
         await page.click(dropdownSelector)
+
+        await delay(10000)
+
+        const files = fs.readdirSync('./jstor_download');
+        files.forEach(file => {
+            console.log(file);
+        });
 
     } catch (error) {
         console.error(error)
