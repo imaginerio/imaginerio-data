588197b74be3f999a8a923ec98d4f32e9d3d47bd
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2024-04-09 11:29:42 -0300

    Fix typo and logic

--- compare_data.py
+++ compare_data.py
@@ -4,11 +4,11 @@ import pandas as pd
 
 current = pd.read_excel("input/jstor.xls")
 
-new = pd.read_excel(os.path.join("jstor_download"), os.listdir("jstor_download")[0])
+new = pd.read_excel(os.path.join("jstor_download", os.listdir("jstor_download")[0]))
 
 comparison = new.compare(current, keep_shape=True)
 
-changes = comparison.notna().any(axis=1).any(level=0)
+changes = comparison.notna().any(axis=1)
 
 to_process = new[changes]
 
